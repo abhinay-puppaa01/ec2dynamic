c14c8360f81cd4dd6e16feafe196cff9acac389a
abhinay-puppaa01 <110231507+abhinay-puppaa01@users.noreply.github.com>
2022-11-10 19:16:28 +0530

    Update mvariable.tf

--- EC2/mvariable.tf
+++ EC2/mvariable.tf
@@ -1,4 +1,84 @@
 
+
+
+
+
+variable "instance_count" {
+  description = "Private IP address to associate with the instance in a VPC"
+  type        = number
+  default     = 1
+}
+
+
+
+
+
+
+variable "iam_instance_profile" {
+  description = "IAM Instance Profile to launch the instance with. Specified as the name of the Instance Profile"
+  type        = string
+  default     = null
+}
+
+variable "ebs_optimized" {
+  description = "If true, the launched EC2 instance will be EBS-optimized"
+  type        = bool
+  default     = null
+}
+
+variable "private_ip" {
+  description = "Private IP address to associate with the instance in a VPC"
+  type        = string
+  default     = null
+}
+variable "tenancy" {
+  description = "The tenancy of the instance (if the instance is running in a VPC). Available values: default, dedicated, host."
+  type        = string
+  default     = null
+}
+variable "host_id" {
+  description = "ID of a dedicated host that the instance will be assigned to. Use when an instance is to be launched on a specific dedicated host"
+  type        = string
+  default     = null
+}
+
+
+
+variable "ebs_block_device_name" {
+  description = "Additional EBS block devices to attach to the instance"
+  type        = list(string)
+  default     = []
+}
+variable "ebs_block_volume_size" {
+  description = "Additional EBS block devices to attach to the instance"
+  type        = list(string)
+  default     = []
+}
+variable "ebs_block_volume_type" {
+  description = "Additional EBS block devices to attach to the instance"
+  type        = list(string)
+  default     = []
+}
+variable "ebs_block_volume_iops" {
+  description = "Additional EBS block devices to attach to the instance"
+  type        = list(string)
+  default     = []
+}
+
+
+variable "root_block_device_volume_type" {
+  description = "root_block_device_volume_type"
+  type        = string
+  default     = "gp2"
+}
+variable "root_block_device_volume_size" {
+  description = "root_block_device_volume_size"
+  type        = string
+  default     = "8"
+}
+
+
+
 variable "access_key" {
   description = "The username of the user account used to access the Morpheus platform"
   type        = string
